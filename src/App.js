f766f7491f93f6610246a0c3d30dc398b13608c7
Min <mtan121@aucklanduni.ac.nz>
2021-08-02 23:26:28 +0800

    Done with assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            number:0,
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+         .then(resp => resp.json())
+         .then(json => {
+             console.log(json)
+             this.setState({
+                 number: json.number
+             })
+         })
+    }
+
+    render() {
+        return <div>{this.state.number}</div>
+    }
+
+
+}
